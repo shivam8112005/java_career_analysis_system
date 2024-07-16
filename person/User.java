749c049315bd2bba01528055ab3eb1fc9440e576
shivam shukla <157223041+shivam8112005@users.noreply.github.com>
2024-07-16 13:49:26 +0530

    Create User.java

---
+++ person/User.java
@@ -0,0 +1,58 @@
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String skills;
+    private String interests;
+    private String personalityTraits;
+
+
+
+     public int getId() {
+         return id;
+     }
+
+     public void setId(int id) {
+         this.id = id;
+     }
+
+     public String getName() {
+         return name;
+     }
+
+     public void setName(String name) {
+         this.name = name;
+     }
+
+     public String getEmail() {
+         return email;
+     }
+
+     public void setEmail(String email) {
+         this.email = email;
+     }
+
+     public String getSkills() {
+         return skills;
+     }
+
+     public void setSkills(String skills) {
+         this.skills = skills;
+     }
+
+     public String getInterests() {
+         return interests;
+     }
+
+     public void setInterests(String interests) {
+         this.interests = interests;
+     }
+
+     public String getPersonalityTraits() {
+         return personalityTraits;
+     }
+
+     public void setPersonalityTraits(String personalityTraits) {
+         this.personalityTraits = personalityTraits;
+     }
+ }
