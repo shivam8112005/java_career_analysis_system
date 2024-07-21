fc176ec6030cf278e992710c0abbaa99a405fae7
shivam8112005 <shivamshukla8112005@gmail.com>
2024-07-21 18:53:17 +0530

    updated edit skill function

--- .src/DataBase/DatabaseUtil.java
+++ .src/DataBase/DatabaseUtil.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class DatabaseUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/career_analysis_system";
@@ -8,6 +9,24 @@ public class DatabaseUtil {
     public static Connection getConnection() throws Exception {
        // Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+    public static void updatingPersonalityTraits(String pt,int id){
+       try {
+        String querry="UPDATE users SET personality_traits = ? WHERE id = ?";
+        PreparedStatement pst=getConnection().prepareStatement(querry);
+        pst.setString(1, pt);
+        pst.setInt(2, id);
+        int r=pst.executeUpdate();
+        if(r>0){
+            System.out.println("Personality Traits updated.");
+        }else{
+            System.out.println("Persoality Traits Not Update !");
+        }
+       } catch (Exception e) {
+        // TODO: handle exception
+        System.out.println(e.getMessage());
+       }
+        
     }
    
 }
